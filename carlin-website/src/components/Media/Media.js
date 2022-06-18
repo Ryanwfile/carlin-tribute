2cfac0ed45ab65b9160531a4b527fce1f8e246ed
Ryan Williams <codereese@gamil.com>
2022-06-18 14:44:57 -0400

    updated and refactored Media component

--- carlin-website/src/components/Media/Media.js
+++ carlin-website/src/components/Media/Media.js
@@ -1,10 +1,35 @@
 import React from "react";
 import Images from "../Images/Images";
+import { Grid } from "@mui/material";
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//     backgroundColor: rgb(14, 9, 9),
+//     whiteSpace: "pre-line",
+//     color: "aliceblue",
+//   },
+//   box: {
+//     display:"flex",
+//     justifyItems: "center"
+//     },
+//   },
+// }));
 
 export default function Media() {
   return (
-    <div>
-      <Images />
+    <div className="media-wrapper">
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          <Images />
+        </Grid>
+      </Grid>
     </div>
   );
 }
