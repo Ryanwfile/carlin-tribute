d14a2fc350e4a674d5327c2a7e8db29a60d4209b
Ryan Williams <codereese@gamil.com>
2022-06-18 14:27:19 -0400

    refactored media component

--- carlin-website/src/components/Images/Images.js
+++ carlin-website/src/components/Images/Images.js
@@ -1,30 +1,106 @@
-import React from "react";
-// import styles from "./Images.module.css";
-import Carousel from "react-material-ui-carousel";
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
 import { CONSTANTS } from "../../constants";
 
-const styles = (theme) => ({
-  Carousel: {
-    width: 300,
-    margin: "auto",
-    height: 250,
-  },
-  // Media: {
-  //   height: 550,
-  //   width: "100%",
-  // },
-});
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+const { IMAGES } = CONSTANTS;
 
-function Images() {
+export default function Images() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = IMAGES.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
   return (
-    <div className={styles.carouselWrapper}>
-      <Carousel interval={100000}>
-        {CONSTANTS.IMAGES.map((image) => {
-          return <img src={image.src} alt="carlin" />;
-        })}
-      </Carousel>
-    </div>
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: 50,
+          pl: 2,
+          bgcolor: "background.default",
+        }}
+      >
+        <Typography>{IMAGES[activeStep].name}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {IMAGES.map((step, index) => (
+          <div key={index}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  // height: childrenHeight,
+                  display: "block",
+                  maxWidth: 400,
+                  overflow: "hidden",
+                  width: "100%",
+                  // setChildrenHeight: setChildrenHeight,
+                }}
+                src={step.src}
+                alt={step.name}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
   );
 }
-
-export default Images;
