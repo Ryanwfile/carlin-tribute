c7c513a4724b6cc1b011ba44cc6d236b8f8859cb
Ryan Williams <codereese@gamil.com>
2022-06-19 08:31:32 -0400

    more swiper updates, about to uninstall less maintained react swipeable views package

--- carlin-website/src/components/CardCustom/CardCustom.js
+++ carlin-website/src/components/CardCustom/CardCustom.js
@@ -1,6 +1,6 @@
 import { Card, CardMedia } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, autoplay } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
 
@@ -8,7 +8,13 @@ import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
 
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// import "swiper/css/scrollbar";
+
 import { CONSTANTS } from "../../constants";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
 const useStyles = makeStyles({
   media: {
@@ -22,11 +28,19 @@ export default function CardCustom(cardCustomProps) {
   return (
     <Card>
       <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
         slidesPerView={3}
         centeredSlides
-        // onSlideChange={() => console.log("slide change")}
-        // onSwiper={(swiper) => console.log(swiper)}
+        grabCursor
+        keyboard={{ enabled: true }}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        navigation
+        loop
+        autoplay={true}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
       >
         {CONSTANTS.IMAGES.map((image, index) => {
           return (
