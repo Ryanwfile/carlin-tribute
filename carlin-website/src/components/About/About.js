1e69d169dddd76b0882e46e5b668e610a7281d85
Ryan Williams <codereese@gamil.com>
2022-06-23 22:31:52 -0400

    more styling

--- carlin-website/src/components/About/About.js
+++ carlin-website/src/components/About/About.js
@@ -1,30 +1,30 @@
 import React from "react";
 import styles from "./About.module.css";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
 import { CONSTANTS } from "../../constants";
-import { Typography } from "@material-ui/core";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  // padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  // paddingLeft: "2rem",
-  // paddingRight: "2rem",
-  // marginLeft: "4rem",
-  // marginRight: "4rem",
-}));
+import { Grid, Typography } from "@material-ui/core";
 
 function About() {
   return (
-    <div className={styles.aboutWrapper}>
-      <Item>
+    <Grid
+      container
+      className={styles.aboutWrapper}
+      justify="center"
+      xs={12}
+      direction="column"
+    >
+      <Grid item>
         <Typography variant="h5">{CONSTANTS.ABOUT.title} </Typography>
-        <Typography variant="body2">{CONSTANTS.ABOUT.content}</Typography>
-      </Item>
-    </div>
+      </Grid>
+      <Grid item>
+        <p
+          style={{
+            fontSize: "",
+          }}
+        >
+          {CONSTANTS.ABOUT.content}
+        </p>
+      </Grid>
+    </Grid>
   );
 }
 
